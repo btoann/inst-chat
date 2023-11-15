0706e0ae227b47dc9bede18210c85f29e6779289
btoann <nguyenbaotoan2001@gmail.com>
2023-11-15 20:53:41 +0700

    refactor

---
+++ components/shared/SideBar/Server.SideBar.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface IServerSideBarProps {
+  serverId: string
+}
+
+const ServerSideBar: FC<IServerSideBarProps> = ({ serverId }) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ServerSideBar
---
+++ components/shared/SideBar/index.ts
@@ -0,0 +1,1 @@
+export { default as ServerSideBar } from './Server.SideBar'
